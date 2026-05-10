a77e4b1d0b64fa3777451f26196f6c3c30a544d4
Gebriel A. Fanta <gebrielabebe@gmail.com>
2026-05-10 19:05:44 +0200

    Add knowledge functionality and update requirements

---
+++ backend/app/knowledge.py
@@ -0,0 +1,28 @@
+import os
+
+from agno.knowledge.embedder.huggingface import HuggingfaceCustomEmbedder
+from agno.knowledge.knowledge import Knowledge
+from agno.vectordb.chroma import ChromaDb
+
+from .config import CHROMA_DIR, HF_TOKEN
+
+os.environ["HF_TOKEN"] = HF_TOKEN
+
+os.makedirs(CHROMA_DIR, exist_ok=True)
+
+vector_db = ChromaDb(
+    collection= 'case_files',
+    path=CHROMA_DIR,
+    persistent_client=True,
+    embedder=HuggingfaceCustomEmbedder()
+)
+
+
+knowledge = Knowledge(name="case_files", vector_db=vector_db)
+
+def add_knowledge(filepath, filename):
+    knowledge.insert(name=filename, path=filepath, skip_if_exists=True)
+
+
+def delete_knowledge(filename):
+    vector_db.delete_by_name(filename)
