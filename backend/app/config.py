1a4b772e1fb1feb98feac493bc7f1ad9c962a193
Gebriel A. Fanta <gebrielabebe@gmail.com>
2026-05-10 18:18:12 +0200

    add config and dependencies

---
+++ backend/app/config.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../../.env"))
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
+USERNAME = os.getenv("USERNAME", "detective")
+PASSWORD = os.getenv("PASSWORD", "sherlock")
+UPLOAD_DIR = os.getenv("UPLOAD_DIR", "./uploads")
+CHROMA_DIR = os.getenv("CHROMA_DIR", "./chroma")
